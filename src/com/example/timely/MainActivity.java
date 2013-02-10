d6f8f5e7ce74ec59768b7d54a0bfb85e89aaf425
Delos Chang <lol.i.laugh@gmail.com>
2013-02-10 16:12:40 -0500

    calling location listener on resume

--- src/com/example/timely/MainActivity.java
+++ src/com/example/timely/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.Menu;
 // deprecate later
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import android.widget.TextView;
 
@@ -22,7 +23,20 @@ public class MainActivity extends Activity {
 	private TextView mLatLng;
 	private LocationManager mLocationManager;
 	
+	private boolean mUseFine;
+	private boolean mUseBoth;
 	
+	// Keys to maintain UI state after rotation.
+	private static final String KEY_FINE = "use_fine";
+	private static final String KEY_BOTH = "use_both";
+	
+    // UI handler codes.
+    private static final int UPDATE_ADDRESS = 1;
+    private static final int UPDATE_LATLNG = 2;
+
+    private static final int TEN_SECONDS = 10000;
+    private static final int TEN_METERS = 10;
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +44,13 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		// We should restore app state (if exists) after rotation
-		//
-		//
+		if (savedInstanceState != null){
+			mUseFine = savedInstanceState.getBoolean(KEY_FINE);
+			mUseBoth = savedInstanceState.getBoolean(KEY_BOTH);
+		} else {
+			mUseFine = false;
+			mUseBoth = false;
+		}
 		
 		mLatLng = (TextView) findViewById(R.id.latlng);
 		
@@ -55,15 +74,58 @@ public class MainActivity extends Activity {
 			// build an alert dialog here that requests the user to enable the location service
 			
 		}
-
+	}
+	
+	protected void onResume(){
+		super.onResume();
+		setup();
 	}
 	
 	@Override
 	protected void onStop(){
 		super.onStop();
+		
+		// remove later for constant location tracking
 		mLocationManager.removeUpdates(listener);
 	}
 
+	@Override
+	// Set up fine/coarse location providers 
+	private void setup(){
+		Location gpsLocation = null;
+		Location networkLocation = null;
+		mLocationManager.removeUpdates(listener);
+		
+		mLatLng.setText(R.string.unknown);
+		
+		if (mUseFine) { 
+			gpsLocation = requestUpdatesFromProvider(
+					LocationManager.GPS_PROVIDER, R.string.not_support_gps);
+			
+			// Update the UI immediately if location is obtained.
+			if (gpsLocation != null){
+				updateUILocation(gpsLocation);
+			}
+		}
+	}
+	
+	/**
+	 * Method to register location updates with desired location provider.
+	 * 
+	 * @param provider Name of requested provider.
+	 * @param errorResId Resource id for the string message to be displayed if provider does not exist on device.
+	 * @return A previously returned {@link android.location.Location} from the requested provider, if exists.
+	 */
+	private Location requestUpdatesFromProvider(final String provider, final int errorResId){
+		Location location = null;
+		if (mLocationManager.isProviderEnabled(provider)){
+			mLocationManager.requestLocationUpdates(provider, TEN_SECONDS, TEN_METERS, listener);
+			location = mLocationManager.getLastKnownLocation(provider);
+		} else {
+			Toast.makeText(this, errorResId, Toast.LENGTH_LONG).show();
+		}
+		return location;
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -84,6 +146,22 @@ public class MainActivity extends Activity {
 	
 	private final LocationListener listener = new LocationListener(){
 		
-	}
+		@Override
+		public void onLocationChanged(Location location){
+			
+			updateUILocation(location);
+		}
+		
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
 
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+	};
 }
